8faa7cffd00b39cf1931026a56e1bdf0b89e7efc
Clodoaldo <clodoaldo.junior.crmj@gmail.com>
2023-05-22 19:03:20 -0300

    novos arquivos

---
+++ Aula 02/conversoes.js
@@ -0,0 +1,19 @@
+// tipo de dado
+//booleanos
+
+//OBS
+
+// "=" => ATRIBUIÇÃO
+// "==" => COMPARANDO O VALOR INTERNO
+// "===" => COMPARANDO VALOR E TIPO
+
+//conversao implicita
+const numero = 456;
+const numeroString = Number("456");
+
+//Number()
+//String()
+console.log(numero + numeroString)
+
+
+//conversao explicita
