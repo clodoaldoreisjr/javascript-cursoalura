55a80462b13ca45814b777fc387b387c90386a79
Clodoaldo <clodoaldo.junior.crmj@gmail.com>
2023-05-22 18:42:33 -0300

    exemplo

---
+++ Aula 01/type-number.js
@@ -0,0 +1,27 @@
+// tipo number
+
+
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+const operacaoMatematica = primeiroNumero / segundoNumero;
+
+console.log(operacaoMatematica);
+
+// ponto flutuante 
+
+const numeroPontoFlutuante = 3.3;
+const pontoFlutuanteSemZero = .5;
+
+const NovaOperacao = primeiroNumero / numeroPontoFlutuante;
+
+console.log(NovaOperacao)
+
+//Nan -> Not a number (nao é um numero)
+
+const alura = "Alura";
+console.log(alura * primeiroNumero)
+
+var a = 10
+var b = 0
+console.log(a/b) // Infinity
---
+++ Aula 01/type-string.js
@@ -0,0 +1,13 @@
+const texto1 = "olá, mundo!";
+const texto2 = 'Olá, mundo!';
+const senha = "senhaSuperSegura456!";
+const StringDeNumeros = "34567";
+
+const citacao = "Meu nome é ";
+const meuNome = "Leonardo";
+
+// concatenacao (+)
+
+console.log(citacao + meuNome);
+
+//template string ou template literal
